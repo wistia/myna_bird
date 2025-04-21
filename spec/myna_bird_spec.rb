57a238bd0b5e4f3252b5bcb29f97f547fce58ecf
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2025-04-21 13:02:04 -0400

    Fix issue where MynaBird would do partial matches of avoided domains. (#5)

--- spec/myna_bird_spec.rb
+++ spec/myna_bird_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe MynaBird do
-
   it_should_convert 'brendan@wistia.com',              to: 'wistia'
   it_should_convert 'brendan.schwartz@gmail.com',      to: 'brendan-schwartz'
   it_should_convert 'support@gmail.com',               to: 'support-at-gmail'
@@ -18,6 +17,7 @@ describe MynaBird do
   it_should_convert 'BRENDAN@WISTIA',                  to: 'wistia'
   it_should_convert 'brendan@outlook.com',             to: 'brendan'
   it_should_convert 'james@outlook.com.br',            to: 'james'
+  it_should_convert 'james@karaol.com',                to: 'karaol'
 
   # bad input
   it_should_not_convert 'no.at.sign'
@@ -26,10 +26,12 @@ describe MynaBird do
   it_should_not_convert '@@@@'
   it_should_not_convert '++@++'
 
-  context 'domain should be avoided' do
-    it 'uses the local name' do
-      MynaBird.avoided_domains = ['post-jazz']
-      MynaBird.convert('davej@post-jazz.no').should == 'davej'
+  describe '.avoided_domains' do
+    before do
+      MynaBird.avoided_domains = ['post-jazz', 'my']
     end
+
+    it_should_convert 'davej@post-jazz.no',            to: 'davej'
+    it_should_convert 'thijs@mychickennuggets.com',    to: 'mychickennuggets'
   end
 end
